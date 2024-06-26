400ccfac5d281a297e9495f080b4c11dfc208941
erickpalomino1923 <127764958+erickpalomino1923@users.noreply.github.com>
2024-06-26 15:04:16 -0500

    Update AppointmentController.java

--- PhysioDesk/src/main/java/physiodesk/physiodesk_backend/productBC/appointments/interfaces/rest/AppointmentController.java
+++ PhysioDesk/src/main/java/physiodesk/physiodesk_backend/productBC/appointments/interfaces/rest/AppointmentController.java
@@ -21,7 +21,7 @@ import java.util.Optional;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
-@CrossOrigin(origins ="*")
+@CrossOrigin(origins ="https://effulgent-lily-2a8d83.netlify.app/")
 @RestController
 @RequestMapping("/api/v1/appointments")
 public class AppointmentController {
